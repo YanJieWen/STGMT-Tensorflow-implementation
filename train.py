e5da48a3dfd3d261e235ed52813d5ffbd28aeacf
YanJieWen <68051908+YanJieWen@users.noreply.github.com>
2022-12-07 12:56:38 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,109 +5,134 @@
 # @FileName : train
 # @Project Name :metacode
 
-# import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 import numpy as np
 from Hyperparameters import Hyperparameters as hp
 from utils_ import *
 from frameworks import *
-from gen_batch import *
+from operations import *
 import time 
 import random
-# #Define GPU
-# config = tf.ConfigProto()
-# config.gpu_options.allow_growth=True
+
+#step1：Prepare dataset pipline
+data = read_pkl(hp.pkl_08)
+train= data[0]
+val = data[1]
+test = data[2]
+tms = toarray(data[3])
+node2vec = data[4]
+scalar = data[5]
+train_dataset = tf.data.Dataset.from_tensor_slices((train[:,:hp.input_len,:,:],train[:,-hp.input_len:,:,:]))
+val_dataset = tf.data.Dataset.from_tensor_slices((val[:,:hp.input_len,:,:],val[:,-hp.input_len:,:,:]))
+train_dataset = train_dataset.cache()#for train dataset
+train_dataset = train_dataset.shuffle(buffer_size=1000).batch(hp.batch_size)
+train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+val_dataset = val_dataset.shuffle(buffer_size=1000).batch(hp.batch_size)
+val_dataset = val_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+#step2：define loss object
+loss_object = tf.keras.losses.MeanAbsoluteError(name='mean_absolute_error')
+def loss_fn(pred,gt):
+    return tf.reduce_mean(loss_object(gt,pred))
+#step3:define metric object
+# def rsqure(gt,pred):
+#     fenzi = tf.reduce_sum(tf.math.square(pred-gt))
+#     fenmu = tf.reduce_sum(tf.math.square(gt-tf.reduce_mean(gt)))
+#     return tf.cast((1-fenzi/fenmu),tf.float32)
+# class R_square(tf.keras.metrics.Metric):
+#     def __init__(self):
+#         super().__init__()
+#         self.total = self.add_weight(name='total', dtype=tf.int32, initializer=tf.zeros_initializer())
+#         self.count = self.add_weight(name='count', dtype=tf.int32, initializer=tf.zeros_initializer())
+#     def update_state(self,gt,pred):
+#         values = rsqure(gt,pred)
+#         self.total.assign_add(tf.reduce_sum(values))
+#         self.count.assign_add(tf.cast(tf.size(gt)),tf.float32)
+#     def result(self):
+#         return self.total/self.count
+train_loss = tf.keras.metrics.MeanSquaredError(name='train_loss')
+#step4:Define optimizer
+learning_rate = CustomSchedule(hp.num_units)
+# optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+#                                      epsilon=1e-9,clipvalue=1.0)
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                     epsilon=1e-9)
+#step5:Define training and checkpoint
+stgmt = STGMT(d_model=hp.num_units,num_heads=hp.num_heads)
+checkpoint_path = hp.ckpt_path
+ckpt = tf.train.Checkpoint(stgmt=stgmt,
+                           optimizer=optimizer)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+# 如果检查点存在，则恢复最新的检查点。
+if ckpt_manager.latest_checkpoint:
+  ckpt.restore(ckpt_manager.latest_checkpoint)
+  print ('Latest checkpoint restored!!')#if future training
+#step6:Define training step
+# train_step_signature = [
+#     tf.TensorSpec(shape=(None, None,None,None), dtype=tf.float32),
+#     tf.TensorSpec(shape=(None, None,None,None), dtype=tf.float32),
+# ]
+@tf.function
+def train_step(enc_inp,dec_inp,n2v, tms):
+    with tf.GradientTape() as tape:
+        final_output, enc_atts, dec_atts = stgmt(enc_inp,dec_inp,n2v,tms,if_te=True,if_se=True,if_ste=True,if_meta=True,training=True)
+        loss = loss_fn(final_output,dec_inp)
+    gradients = tape.gradient(loss, stgmt.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, stgmt.trainable_variables))
+    train_loss(dec_inp,final_output)
+    # train_accuarcy()
+#eval function
+def evaluate(enc_inp,n2v,tms):
+    b,t,n,d = get_shape(enc_inp)
+    dec_inp = tf.zeros((b,hp.output_len,n,d))
+    for step in range(dec_inp.shape[1]):
+        outs, _, _ = stgmt(enc_inp, dec_inp, n2v, tms, if_te=True, if_se=True, if_ste=True,
+                                                 if_meta=True, training=False)
+        outs_ = outs.numpy()
+        dec_inp_ = dec_inp.numpy()
+        dec_inp_[:, step, :, :] = outs_[:, step, :, :]
+        dec_inp = tf.convert_to_tensor(dec_inp_,tf.float32)
+        # dec_inp_list = tf.unstack(dec_inp)
+        # outs_list = tf.unstack(outs)
+        # dec_inp_list[:,step,:,:]=outs_list[:,step,:,:]
+        # dec_inp = tf.stack(dec_inp_list)
+    return dec_inp#->(b,t,n,d)
 
 
-#load all datasets needed in framework
-all_datas = read_pkl(hp.pkl_08)
-train_data = all_datas[0]#gen by yield
-val_data = all_datas[1]
-zones = val_data.shape[2]
-tms = toarray(all_datas[3])
-num_sensors = train_data.shape[2]
-if hp.if_onlydist:
-    sim_matrix = toarray([tms[0]])#keep_dims
-elif hp.if_onlytimesim:
-    sim_matrix = toarray([tms[1]])
-else:
-    sim_matrix = tms
-node2vec = all_datas[4]
-scalar = all_datas[5]
-predictions = np.zeros_like(val_data[:,hp.input_len:(hp.input_len+hp.output_len),:,:])
-#load end
-#Define the input datasets
-x = tf.placeholder(dtype=tf.float32, shape=[None, hp.input_len, num_sensors, hp.out_units])  # ->(B,T,N,C)
-y = tf.placeholder(dtype=tf.float32, shape=[None, hp.output_len, num_sensors, hp.out_units])  # ->(B,T2,N,C)
-nodev = tf.placeholder(dtype=tf.float32, shape=[num_sensors, hp.d_node])  # ->(N,dc)
-transition_matrices = tf.placeholder(dtype=tf.float32, shape=[sim_matrix.shape[0], num_sensors, num_sensors])#need to point a dims 
-#build model
-meta_transformer =Meta_transformer(x,y,nodev,transition_matrices)
-enc_memory,src_mask,enc_stes = meta_transformer.encoder(if_training=True)
-y_hat = meta_transformer.decoder(enc_memory,src_mask,enc_stes,if_training=True)
-if hp.if_val:
-    enc_memory_, src_mask_, enc_stes_ = meta_transformer.encoder(if_training=False)
-    val_pred = meta_transformer.decoder(enc_memory_, src_mask_, enc_stes_,if_training=False)
-#Define loss
-loss = tf.losses.mean_squared_error(y,y_hat)#genearl datasets
-# loss = tf.nn.l2_loss(y-y_hat)
-# loss = tf.reduce_mean(tf.abs(y-y_hat))#senstive datsets
-global_step = tf.train.get_or_create_global_step()#Create a global variable to record the number of steps
-lr = tf.train.exponential_decay(hp.lr, global_step ,
-decay_steps=5 * (len(train_data)//hp.batch_size), decay_rate=0.7, staircase=True)
-optimizer = tf.train.AdamOptimizer(lr)
-train_op = optimizer.minimize(loss, global_step=global_step)
-tf.summary.scalar('lr', lr)
-tf.summary.scalar("loss", loss)
-tf.summary.scalar("global_step", global_step)
-summaries = tf.summary.merge_all()
-#start session
-saver = tf.train.Saver(max_to_keep=3)
-# with tf.Session(config=config) as sess:
-with tf.Session() as sess:
-    writer = tf.summary.FileWriter(hp.logdir, sess.graph)
-    sess.run(tf.global_variables_initializer())
-    # saver.restore(sess,tf.train.latest_checkpoint('ckpt')) #future training
-    init_error = hp.valid_thresh
-    for i in range(hp.num_epochs):
-        for j, inputs_ in enumerate(gen_batch(train_data,hp.batch_size)):
-            start_time = time.time()
-            _, _gs, summary, loss_ = sess.run([train_op, global_step, summaries, loss],
-                                              feed_dict={x: inputs_[:, :hp.input_len,:,:],
-                                                         y: inputs_[:, hp.input_len:(hp.input_len + hp.output_len),:,:],
-                                                         nodev:node2vec,
-                                                         transition_matrices:sim_matrix})
-            writer.add_summary(summary, _gs)
-            # autoregression for each step
-            if j%20==0:
-        # autoregression for each step
-                if hp.if_val:  # set value is False to avoid memoryout
-                    len_data = len(val_data)
-                    idx = np.arange(len_data)
-                    np.random.shuffle(idx)
-                    id = random.choice(idx)
-                    start_idx = id
-                    end_idx = id+hp.batch_size
-                    if end_idx>len_data:
-                        end_idx = len_data
-                    slc = idx[start_idx:end_idx]
-                    for step in range(hp.output_len):
-                        _predictions = sess.run(val_pred, feed_dict={x: val_data[slc,:hp.input_len,:,:], y: predictions[slc],
-                                                                    nodev:node2vec,
-                                                                    transition_matrices:sim_matrix})
-                        predictions[slc,step,:,:] = _predictions[:, step,:,:]
-                    predictions_0 = inverse_minmaxsca(predictions[slc,:,:,:].reshape(-1,zones,hp.out_units),scalar)
-                    gt_0 = inverse_minmaxsca(val_data[slc,hp.input_len:,:,:].reshape(-1,zones,hp.out_units),scalar)#the first time point
-                    error = np.mean(np.abs(predictions_0-gt_0))
-                    print('The validation MAE value is {:.2f}, for the {} steps in {} epochs.'.format(error,j,i))
-                    if error<=init_error:
-                        init_error = error
-                        saver.save(sess=sess, save_path=hp.ckpt_path, global_step=(i + 1))
-                else:
-                    if i % 10 == 0 and j % (20 * 3) == 0 or i == hp.num_epochs - 1:
-                        saver.save(sess=sess, save_path=hp.ckpt_path, global_step=(i + 1))
-                print('The current loss value is {:.5f}, for the {} steps in {} epochs.Per step use {:.2f} s'.format(loss_, j, i,time.time()-start_time))
-    sess.close()
+#train processing
+init_val = hp.init_val
+for epoch in range(hp.num_epochs):
+    start = time.time()
+    train_loss.reset_states()
+    # train_accuarcy.update_state(gt,pred)
+    for (batch,(enc_inp,dec_inp)) in enumerate(train_dataset):
+        train_step(enc_inp,dec_inp,node2vec, tms)
+        if batch%50==0:
+            print('Epoch {} /Batch {} Loss {:.4f}'.format(
+                epoch + 1, batch, train_loss.result()))
+        if (epoch+1)%5==0 and batch%50==0:
+            if hp.if_val:#if use validation data
+                count = 0
+                errors = []
+                while count<10:
+                    enc_inp,gt = next(iter(val_dataset))
+                    pred_ = evaluate(enc_inp,node2vec,tms)
+                    pred = scalar.inverse_transform(tf.reshape(pred_,[-1,hp.out_units]))
+                    gt = scalar.inverse_transform(tf.reshape(gt,[-1,hp.out_units]))
+                    mae = tf.reduce_mean(tf.abs(pred-gt))
+                    errors.append(mae)
+                    count+=1
+                mae = tf.reduce_mean(tf.convert_to_tensor(errors,tf.float32))
+                if mae.numpy()<init_val:
+                    ckpt_save_path = ckpt_manager.save()
+                    print('Saving checkpoint for epoch {} at {}-->MAE is {:.4f}'.format(epoch + 1,
+                                                                       ckpt_save_path,mae.numpy()))
+                    init_val = mae.numpy()
+
+            else:
+                ckpt_save_path = ckpt_manager.save()
+                print('Saving checkpoint for epoch {} at {}'.format(epoch + 1,
+                                                                    ckpt_save_path))
+    print('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
+
 
 
